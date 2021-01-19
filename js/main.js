063ed2d3f5bb0dc3e2277eb15b518a4ffc55c482
Hoochie <38121436+jayhutch17@users.noreply.github.com>
2021-01-19 10:27:30 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+src = "https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"
+    // TODO: Add SDKs for Firebase products that you want to use
+
+
+
+function myFunction() {
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCAlsM7EGPEV-v0-t-Qmt2EhuU-lr9Gdaw",
+        authDomain: "my-test-app-2353d.firebaseapp.com",
+        projectId: "my-test-app-2353d",
+        storageBucket: "my-test-app-2353d.appspot.com",
+        messagingSenderId: "230947206272",
+        appId: "1:230947206272:web:5fc489c3a18d0e4b17e265"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+}
